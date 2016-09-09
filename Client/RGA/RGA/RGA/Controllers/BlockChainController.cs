c6c59be0e5760a88109b52ab1d7ca1f5d1bbf8af
Alex Gillette <alexgillette1991@gmail.com>
2016-09-09 13:59:48 -0500

    added blockchain controller mock, added person class

---
+++ Client/RGA/RGA/RGA/Controllers/BlockChainController.cs
@@ -0,0 +1,48 @@
+﻿using RGA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RGA.Controllers
+{
+    public class BlockChainController : ApiController
+    {
+        [ActionName("GetPerson")]
+        [HttpPost]
+        public Person GetPerson(string name)
+        {
+            return new Person { Name = name };
+        }
+
+        [ActionName("AddPerson")]
+        [HttpPost]
+        public bool AddPerson(Person person)
+        {
+            return true;
+        }
+
+        [ActionName("AddPerson")]
+        [HttpPost]
+        public bool AddHeartBeat(Person person, int heartBeat)
+        {
+            return true;
+        }
+
+        [ActionName("AddWalkingDistance")]
+        [HttpPost]
+        public bool AddWalkingDistance(Person person, int walkingDistance)
+        {
+            return true;
+        }
+
+        [ActionName("AddRunningDistance")]
+        [HttpPost]
+        public bool AddRunningDistance(Person person, int runningDistance)
+        {
+            return true;
+        }
+    }
+}
